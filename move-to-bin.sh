8b08b0fd779915171854a8e9c81a94d8091436ac
aireilly <aireilly@redhat.com>
2023-08-01 12:12:18 +0100

    updates

---
+++ move-to-bin.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+source_dir="."
+
+dest_dir="$HOME/bin"
+
+for file in "$source_dir"/*.sh; do
+  if [ "$file" != "$source_dir/move-to-bin.sh" ]; then
+    mv "$file" "$dest_dir/"
+  fi
+done
+
+sudo chmod a+x "$dest_dir"/*.sh
+
+echo "Moved and made *.sh files executable in $dest_dir"
